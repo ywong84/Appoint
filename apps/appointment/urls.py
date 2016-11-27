5979b5b7d6d461c7836b02dee30e8af29dc329cb
Wallis Wong <ywong84@gmail.com>
2016-11-26 20:39:56 -0800

    initial commit

---
+++ apps/appointment/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^create$', views.create),
+    url(r'^login$', views.login),
+    url(r'^dashboard$', views.dashboard),
+    url(r'^logout$', views.logout),
+    url(r'^add_appointment/(?P<user_id>\d+?)$', views.create_appointment),
+    url(r'^processappointment$', views.processappointment),
+    url(r'^delete/(?P<user_id>\d+?)$', views.delete),
+    url(r'^edit/(?P<user_id>\d+?)$', views.edit)
+
+]
